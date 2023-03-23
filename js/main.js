9f856e8f97f80a4c424a0d37551e4d408057db78
Marco Rinaldi <marcorinaldi98@libero.it>
2023-03-23 14:00:11 +0100

    esercizio base completato

--- js/main.js
+++ js/main.js
@@ -7,7 +7,16 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            email: []
         }
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (response) => {
+            console.log(response.data);
+            this.email.push(response.data.response);
+            }); 
+        }      
     }
 }).mount('#app')
